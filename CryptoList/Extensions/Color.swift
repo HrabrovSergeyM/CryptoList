a88947ec891f8841a4ee0205aaab0a4c58bc0207
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-18 07:42:20 +0300

    main: Added assets

---
+++ CryptoList/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CryptoList
+//
+//  Created by Sergey Hrabrov on 18.07.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
