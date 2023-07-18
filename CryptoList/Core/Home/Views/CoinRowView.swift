1cad5fd61ee8973301d6ee66352a063b1ca68583
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-18 10:28:22 +0300

    main: Code refactoring

--- CryptoList/Core/Home/Views/CoinRowView.swift
+++ CryptoList/Core/Home/Views/CoinRowView.swift
@@ -19,6 +19,31 @@ struct CoinRowView: View {
     
     var body: some View {
         HStack(spacing: 0) {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                centerColumn
+            }
+            
+            rightColumn
+            
+        } // HStack
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+    }
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
             Text("\(coin.rank)")
                 .font(.caption)
                 .foregroundColor(Color.theme.secondaryText)
@@ -29,36 +54,30 @@ struct CoinRowView: View {
                 .font(.headline)
                 .padding(.leading, 6)
                 .foregroundColor(Color.theme.accent)
-            Spacer()
-            
-            if showHoldingsColumn {
-                VStack(alignment: .trailing) {
-                    Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
-                        .bold()
-                    Text((coin.currentHoldings ?? 0).asNumberString())
-                } // VStack
-                .foregroundColor(Color.theme.accent)
-            }
-            
-            VStack(alignment: .trailing) {
-                Text(coin.currentPrice.asCurrencyWith6Decimals())
-                    .bold()
-                    .foregroundColor(Color.theme.accent)
-                Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
-                    .foregroundColor(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                        Color.theme.green :
-                            Color.theme.red
-                    )
-            } // VStack
-            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
         } // HStack
-        .font(.subheadline)
     }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        } // VStack
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                        Color.theme.red
+                )
+        } // VStack
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
     }
 }
