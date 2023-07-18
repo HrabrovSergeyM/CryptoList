98c9d00f5c69cc815e04da05231607a5558d5258
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-18 07:24:45 +0300

    Initial Commit

---
+++ CryptoList/CryptoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoListApp.swift
+//  CryptoList
+//
+//  Created by Sergey Hrabrov on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
